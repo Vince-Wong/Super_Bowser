2280c0b37c1e07eefbe6f17473b80789972e7cea
Pk <Pk@10.0.0.19>
2015-06-25 16:40:17 -0700

    Fixed test drops

--- src/game_logic/Room1.java
+++ src/game_logic/Room1.java
@@ -44,8 +44,7 @@ public class Room1 extends WorldTemplate
 		mobs = new ArrayList<>();
 		mobs.add(new MobStand("practice mob1", 1, 3));
 		mobs.add(new MobStand("practice mob2", 3, 6));
-		mobs.add(new MobStand("practice mob3", 7, 6));
-		mobs.add(new MobStand("practice mob4", 5, 3));
+		
 
 		oldLady.setID(1);
 		oldLadyI = new Image("res/OldLady.png");
